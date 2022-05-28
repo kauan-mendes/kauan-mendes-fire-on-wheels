9f764a25895a28b835a8c34640db559cf451c4f9
Kauan Mendes <kauan.mendes@sptech.school>
2022-05-28 15:18:43 -0300

    Mudando os cards de erro

--- public/js/vetorimg.js
+++ public/js/vetorimg.js
@@ -1,5 +1,5 @@
 function mudanca_tela_login(){
-    var motos= ['url("https://wallpaperaccess.com/full/7202890.jpg")', 'url("https://s1.1zoom.me/big3/143/388615-blackangel.jpg")', 'url("https://images4.alphacoders.com/766/thumb-1920-76626.jpg")', 'url("https://consorciolemes.com.br/wp-content/uploads/2020/03/Honda_motorcycles-HD_Desktop_Wallpaper_2560x1600.jpg")','url("https://s2.best-wallpaper.net/wallpaper/1920x1080/1111/Suzuki-GSXR-600-motorcycle_1920x1080.jpg")','url("https://cutepics.net/wp-content/uploads/2020/11/moto-wallpaper-4k-free-download-for-desktop-background-23-scaled.jpg")']
+    var motos= ['url("./assets/imgs/BETOR8.jpg")', 'url("./assets/imgs/BETOR2.jpg")', 'url("./assets/imgs/BETOR3.jpg")', 'url("./assets/imgs/BETOR7.jpg")','url("./assets/imgs/BETOR5.jpg")','url("./assets/imgs/BETOR6.jpg")']
 
 
    var computador = Math.floor(Math.random(0) * (6));
